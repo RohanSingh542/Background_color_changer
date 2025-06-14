840ec3859b01696103cbc95d46c7c6014a7702cc
RohanSingh542 <139442790+RohanSingh542@users.noreply.github.com>
2025-06-14 23:49:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ colorEle.forEach(function(box){
     box.addEventListener('click', function(color){
         if(color.target.id === 'box1'){
             body.style.backgroundColor = '#FF7D29'
+            colorContainerEle.style.backgroundColor = '#FF7D29'
         }
         if(color.target.id === 'box2'){
             body.style.backgroundColor = '#B6F500'
+            colorContainerEle.style.backgroundColor = '#B6F500'
         }
         if(color.target.id === 'box3'){
             body.style.backgroundColor = '#725CAD'
+            colorContainerEle.style.backgroundColor = '#725CAD'
         }
         if(color.target.id === 'box4'){
             body.style.backgroundColor = '#4DA8DA'
+            colorContainerEle.style.backgroundColor = '#4DA8DA'
         }
         if(color.target.id === 'box5'){
             body.style.backgroundColor = '#D50B8B'
+            colorContainerEle.style.backgroundColor = '#D50B8B'
         }
     })
 })
